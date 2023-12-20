9dce315612f2236cee7a2735d57207dbd6e97c99
Azmal <azmalgazi8@gmail.com>
2023-12-20 20:41:32 +0600

    Practice javascript fuction, dom

---
+++ javascript_refresher.js
@@ -0,0 +1,74 @@
+/* var lang = "Bangla";
+
+function learn(topic){
+   var lang = topic;
+
+   if(true) {
+    let a = 5;
+    a =7
+    console.log(a);
+   }
+
+    console.log(`I am learning ${topic}`);
+}
+
+learn("Javascript")
+console.log(`I know ${lang}`) */
+
+
+
+// regular function
+/* function hello(){
+    console.log("Hello Word");
+}
+
+let msg = hello()
+console.log(msg)
+
+ */
+
+//function expression
+/* const hello = function(){
+    console.log("Hello Word");
+}
+console.log(hello) */
+
+
+// named function expression
+/* const hello = function hello(){
+    console.log("Hello Word");
+}
+console.log(hello) */
+
+
+// Arrow function
+
+/* const hello = () =>{
+    console.log("Hello word");
+}
+
+const ami = (a, b) => {
+    a++;
+    return a+ b;
+}
+
+console.log(ami(5,6)) */
+
+//anonymous function
+
+// function hello(){
+//     return function(){
+//         console.log("Hello word");
+//     }
+// }
+
+
+
+//  -------- Event Hnadler --------------------------------
+
+function hello(){
+    console.log("Hello Tumi")
+}
+
+
+const button = document.createElement("")
